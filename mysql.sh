22df09b8ff8daba9555d3c7a3f6869abb7861c89
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-04 22:26:31 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+mkdir  $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
