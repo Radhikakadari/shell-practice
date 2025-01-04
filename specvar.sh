817e55b6b86cccaa1b317371239d36a12f0bb2ef
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-04 21:39:50 +0530

    hi

--- specvar.sh
+++ specvar.sh
@@ -3,8 +3,8 @@
 echo "all variables passed to the script $@"
 echo "Number of variables passed to it $#"
 echo "script name $0"
-echo "current working directory $pwd"
-echo "home directory of current user $home"
+echo "current working directory $PWD"
+echo "home directory of current user $HOME"
 echo "pid of the script executing now $$"
 echo "pid of the last background command $!"
 sleep 100 &
